702225aeaa9d67d23059478ba5219d1c5efd2972
Richardson25 <egaliterichardson@gmail.com>
2024-10-21 17:56:39 -0700

    Project_React1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div className="app">
+      <img src="react.png" alt="logo" />
+      <h1>I learn react</h1>
+      <p>React is amazing</p>
+      <div>
+        <button>My button</button>
+        <button>I love react</button>
+      </div>
+      <hr />
+      <h2> Do you like react?</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet animi
+        optio deserunt dolor quasi et cum! Qui omnis consectetur, nulla, velit
+        minus quod facilis mollitia magni voluptatum at corporis repellat!
+      </p>
+    </div>
+  );
+}
